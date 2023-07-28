9f300a7e9645807081cb49dfb3239cd5f30e46b8
Tristan F <leodog896@gmail.com>
2023-07-28 14:34:38 -0400

    chore: init, svelvet

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Node, Svelvet } from 'svelvet';
+
+  let width: number;
+  let height: number;
+</script>
+
+<svelte:window bind:innerWidth={width} bind:innerHeight={height}></svelte:window>
+
+<Svelvet {width} {height} TD minimap controls>
+  <Node />
+  <Node label="Default Node" />
+</Svelvet>
